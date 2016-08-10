2e2f9f47837ac1e5651c073deaa272da8fa38e2f
Mustafa Al-Ameen <mustaprox@gmail.com>
2016-08-10 19:42:31 +0300

    Initial Commit

---
+++ SNBtoTXT/MainForm.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace SNBtoTXT
+{
+    partial class SNBtoTXTConverter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SNBtoTXTConverter));
+            this.itemsList = new System.Windows.Forms.ListBox();
+            this.lblDrag = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.fbd = new System.Windows.Forms.FolderBrowserDialog();
+            this.menu = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusBar = new System.Windows.Forms.StatusStrip();
+            this.stLbl = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menu.SuspendLayout();
+            this.statusBar.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // itemsList
+            // 
+            this.itemsList.BackColor = System.Drawing.Color.White;
+            this.itemsList.FormattingEnabled = true;
+            this.itemsList.Location = new System.Drawing.Point(5, 40);
+            this.itemsList.Name = "itemsList";
+            this.itemsList.Size = new System.Drawing.Size(413, 199);
+            this.itemsList.TabIndex = 0;
+            this.itemsList.KeyDown += new System.Windows.Forms.KeyEventHandler(this.itemsList_KeyDown);
+            // 
+            // lblDrag
+            // 
+            this.lblDrag.AutoSize = true;
+            this.lblDrag.Location = new System.Drawing.Point(1, 24);
+            this.lblDrag.Name = "lblDrag";
+            this.lblDrag.Size = new System.Drawing.Size(236, 13);
+            this.lblDrag.TabIndex = 1;
+            this.lblDrag.Text = "Drag and drop the files you want to add here...";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Enabled = false;
+            this.btnStart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStart.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.btnStart.Location = new System.Drawing.Point(5, 244);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(413, 35);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start Converting";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // menu
+            // 
+            this.menu.BackColor = System.Drawing.Color.DodgerBlue;
+            this.menu.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menu.Location = new System.Drawing.Point(0, 0);
+            this.menu.Name = "menu";
+            this.menu.Size = new System.Drawing.Size(423, 24);
+            this.menu.TabIndex = 3;
+            this.menu.Text = "menu";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fileToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(95, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.aboutToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // statusBar
+            // 
+            this.statusBar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.stLbl});
+            this.statusBar.Location = new System.Drawing.Point(0, 285);
+            this.statusBar.Name = "statusBar";
+            this.statusBar.Size = new System.Drawing.Size(423, 22);
+            this.statusBar.TabIndex = 4;
+            this.statusBar.Text = "statusStrip1";
+            // 
+            // stLbl
+            // 
+            this.stLbl.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stLbl.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.stLbl.Name = "stLbl";
+            this.stLbl.Size = new System.Drawing.Size(43, 17);
+            this.stLbl.Text = "Ready.";
+            // 
+            // SNBtoTXTConverter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(423, 307);
+            this.Controls.Add(this.statusBar);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblDrag);
+            this.Controls.Add(this.itemsList);
+            this.Controls.Add(this.menu);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menu;
+            this.MaximizeBox = false;
+            this.Name = "SNBtoTXTConverter";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Samsung® Notes .snb to .txt Converter";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.menu.ResumeLayout(false);
+            this.menu.PerformLayout();
+            this.statusBar.ResumeLayout(false);
+            this.statusBar.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox itemsList;
+        private System.Windows.Forms.Label lblDrag;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.FolderBrowserDialog fbd;
+        private System.Windows.Forms.MenuStrip menu;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusBar;
+        private System.Windows.Forms.ToolStripStatusLabel stLbl;
+    }
+}
+
---
+++ SNBtoTXT/MainForm.cs
@@ -0,0 +1,149 @@
+﻿using Ionic.Zip;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace SNBtoTXT
+{
+    public partial class SNBtoTXTConverter : Form
+    {
+        public SNBtoTXTConverter()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            itemsList.AllowDrop = true;
+            itemsList.DragDrop += itemsList_DragDrop;
+            itemsList.DragEnter += itemsList_DragEnter;
+        }
+
+        private void itemsList_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.Copy;
+        }
+
+        private void itemsList_DragDrop(object sender, DragEventArgs e)
+        {
+            try
+            {
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                foreach (string file in files)
+                {
+                    if (!string.IsNullOrEmpty(Path.GetExtension(file)))
+                    {
+                        itemsList.Items.Add(file);
+                    }
+                }
+
+                stLbl.Text = string.Format("{0} item(s) to convert.", itemsList.Items.Count);
+
+                if (itemsList.Items.Count > 0)
+                {
+                    btnStart.Enabled = true;
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("An unexpected error occured. \r\nError information: " + exc.Message, "Exception", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            Convert();
+        }
+
+        private void Convert()
+        {
+            try
+            {
+                string result = fbd.ShowDialog().ToString();
+                if (result == "OK")
+                {
+                    foreach (string item in itemsList.Items)
+                    {
+                        using (ZipFile zip = ZipFile.Read(item))
+                        {
+                            ZipEntry ee = zip["snote\\snote.xml"];
+                            List<string> xmlContents = new List<string>();
+                            using (var ms = new MemoryStream())
+                            {
+                                ee.Extract(ms);
+                                ms.Position = 0;
+                                var sr = new StreamReader(ms);
+                                var myStr = sr.ReadToEnd();
+                                xmlContents.Add(myStr);
+                            }
+
+                            for (int i = 0; i < xmlContents.Count; i++)
+                            {
+                                string xmlResult = "";
+                                using (XmlReader reader = XmlReader.Create(new StringReader(xmlContents[i])))
+                                {
+                                    XmlWriterSettings ws = new XmlWriterSettings();
+                                    ws.Indent = true;
+                                    reader.ReadToFollowing("sn:t");
+                                    xmlResult = reader.ReadElementContentAsString().Replace("\n", "\r\n");
+                                    StreamWriter sw = new StreamWriter(new FileStream(fbd.SelectedPath + "\\" + Path.GetFileNameWithoutExtension(item) + ".txt", FileMode.Create), Encoding.UTF8);
+                                    sw.Write(xmlResult);
+                                    sw.Close();
+                                }
+                            }
+                        }
+                    }
+                    stLbl.Text = string.Format("{0} item(s) converted successfuly.", itemsList.Items.Count);
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("An unexpected error occured. \r\nError information: " + exc.Message, "Exception", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Samsung® Notes .snb to .txt Converter, made by: Mustafa (@MuStAPro).\r\nSamsung® is a trademark for Samsung Electronics Limited.", "About", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void itemsList_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Delete)
+            {
+                if (itemsList.Items.Count > 1)
+                {
+                    int tmpIdx = itemsList.SelectedIndex;
+                    itemsList.Items.RemoveAt(itemsList.SelectedIndex);
+                    if (tmpIdx + 1 > itemsList.Items.Count)
+                    {
+                        itemsList.SelectedIndex = itemsList.Items.Count - 1;
+                    }
+                    else
+                    {
+                        itemsList.SelectedIndex = tmpIdx;
+                    }
+                }
+                else
+                {
+                    itemsList.Items.RemoveAt(itemsList.SelectedIndex);
+                }
+
+                stLbl.Text = "Ready.";
+
+                if (itemsList.Items.Count == 0)
+                {
+                    btnStart.Enabled = false;
+                }
+            }
+        }
+    }
+}
